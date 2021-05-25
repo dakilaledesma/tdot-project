0f11792f838e52988072aa852b53a599cf537dec
Dakila Ledesma <37383659+dakilaledesma@users.noreply.github.com>
2021-05-25 13:27:47 -0400

    Remove old functions and code, add data

--- server.py
+++ server.py
@@ -4,10 +4,13 @@ import socket
 socket.setdefaulttimeout(4000)
 
 from flask import Flask, render_template, get_template_attribute
+import pandas as pd
 import flask
 
 app = Flask(__name__)
 
+precip_df = pd.read_csv("data/precip.dat", delimiter="  ", engine="python")
+print(precip_df.head())
 
 @app.route('/')
 def index():
@@ -15,7 +18,7 @@ def index():
 
 @app.route('/predict')
 def predict():
-
+    pass
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, threaded=True, debug=False)
